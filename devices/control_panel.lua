f4014678806fd828d98a0029c8ade24caa328bee
Denver Cox <denvercox28@gmail.com>
2025-08-13 12:25:35 -0400

    Changes to be committed: 	new file:   devices/ControlPanel.tga 	new file:   devices/control_panel.lua 	new file:   devices/device_list.lua 	new file:   ui/textures/HUD/HUD-ControlPanel-A.tga 	new file:   ui/textures/HUD/HUD-ControlPanel-D.tga 	new file:   ui/textures/HUD/HUD-ControlPanel-R.tga 	new file:   ui/textures/HUD/HUD-ControlPanel-S.tga 	new file:   ui/textures/HUD/detail/HUD-Details-ControlPanel.tga

---
+++ devices/control_panel.lua
@@ -0,0 +1,48 @@
+ConstructEffect = "effects/device_construct.lua"
+CompleteEffect = "effects/device_complete.lua"
+DestroyUnderwaterEffect = "mods/dlc2/effects/device_explode_submerged.lua"
+Scale = 1
+SelectionWidth = 44.0
+SelectionHeight = 40.0
+SelectionOffset = { 0.0, -40.5 }
+Mass = 40.0
+HitPoints = 250.0
+EnergyProductionRate = 0.0
+MetalProductionRate = 0.0
+EnergyStorageCapacity = 0.0
+MetalStorageCapacity = 0.0
+MinWindEfficiency = 0
+MaxWindHeight = 0
+MaxRotationalSpeed = 0
+DrawBracket = false
+DrawBehindTerrain = true
+NoReclaim = false
+TeamOwned = true
+BlockPenetration = false
+
+dofile("effects/device_smoke.lua")
+SmokeEmitter = StandardDeviceSmokeEmitter
+
+Sprites =
+{
+	{
+		Name = "control_panel-base",
+		States =
+		{
+			Normal = { Frames = { { texture = path .. "/devices/ControlPanel.tga" }, mipmap = true, }, },
+		},
+	},
+}
+
+Root =
+{
+	Name = "ControlPanel",
+	Angle = 0,
+	Pivot = { 0, -0.57 },
+	PivotOffset = { 0, 0 },
+	Sprite = "control_panel-base",
+
+	ChildrenInFront =
+	{
+	},
+}
---
+++ devices/device_list.lua
@@ -0,0 +1,36 @@
+
+
+table.insert(Sprites, DetailSprite("hud-detail-control-panel", "HUD-Details-ControlPanel", path))
+table.insert(Sprites, ButtonSprite("hud-control-panel-icon", "HUD/HUD-ControlPanel", nil, ButtonSpriteBottom, nil, nil, path))
+
+function IndexOfDevice(saveName)
+	for k,v in ipairs(Devices) do
+		if v.SaveName == saveName then
+			return k
+		end
+	end
+	return #Devices
+end
+
+table.insert(Devices, IndexOfDevice("sandbags") + 1,
+{
+	SaveName = "control_panel",
+	FileName = path .. "/devices/control_panel.lua",
+	Icon = "hud-control-panel-icon",
+	Detail = "hud-detail-control-panel",
+	Prerequisite = "upgrade",
+	BuildTimeComplete = 30,
+	ScrapPeriod = 8,
+	MetalCost = 150,
+	EnergyCost = 600,
+	MetalRepairCost = 150,
+	EnergyRepairCost = 600,
+	MetalReclaimMin = 0,
+	MetalReclaimMax = 0,
+	EnergyReclaimMin = 0,
+	EnergyReclaimMax = 0,
+	MaxUpAngle = StandardMaxUpAngle,
+	BuildOnGroundOnly = false,
+	HasDummy = false,
+	SelectEffect = "ui/hud/devices/ui_devices",
+})
